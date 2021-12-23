03ff901f19a848c0b5d851625911feaa6b337e1d
ChristianRojasOliver <christian.rojas201@gmail.com>
2021-12-23 03:20:39 +0000

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,15 +1,7 @@
--- create mysql server for application --
-
--- create main db --
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- create main user --
+USE hbnb_dev_db;
+ 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- user has full priviledges on main db --
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- select priviledge for user on performance schema db --
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
-FLUSH PRIVILEGES;
