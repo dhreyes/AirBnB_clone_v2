056c418e0e9b4e679386e342bed37c613e9e5868
dhreyes <2590@holbertonschool.com>
2022-02-02 21:20:13 -0400

    pep8 fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,8 @@ web application must be listening on 0.0.0.0, port 5000.
 Routes:
 /: display "Hello HBNB!"
 /hbnb: display "HBNB"
-/c/<text>: display "C" followed by the value of the text variable (replace underscore _ sympols with a space )
+/c/<text>: display "C" followed by the value of the 
+text variable (replace underscore _ sympols with a space )
 Must use the option strict_slashes=False
 """
 from flask import Flask
