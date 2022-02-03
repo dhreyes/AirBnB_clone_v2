5f241ae94e78885c9f53c37760de01d9d0f3c9f4
dhreyes <2590@holbertonschool.com>
2022-02-02 23:30:21 -0400

    Add missing route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,6 +31,7 @@ def c(text):
     return ("C " + text.replace("_", " "))
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     return ("Python " + text.replace("_", " "))
