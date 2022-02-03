1a799495e39d0f25364b6ea5f4d1223617fc263f
dhreyes <2590@holbertonschool.com>
2022-02-02 23:27:03 -0400

    pep8 fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,7 +5,7 @@ web application must be listening on 0.0.0.0, port 5000.
 Routes:
 /: display "Hello HBNB!"
 /hbnb: display "HBNB"
-/c/<text>: display "C" followed by the value of the 
+/c/<text>: display "C" followed by the value of the
 text variable (replace underscore _ sympols with a space )
 /python/(<text>): display "Python" followed by the value of the
 text variable (replace underscore _ symbols with a space )
