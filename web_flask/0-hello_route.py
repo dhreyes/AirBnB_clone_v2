125abdbcaf5bbaafe29a2d5ad791a8156d6a9641
dhreyes <2590@holbertonschool.com>
2022-02-03 00:43:30 +0000

    pep8 fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,9 +9,11 @@ Must use the option strict_slashes=False in route definition
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run()
